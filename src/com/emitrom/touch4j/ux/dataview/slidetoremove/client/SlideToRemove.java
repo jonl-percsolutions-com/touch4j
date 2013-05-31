de3fb935b5f02bdbf91294b9cfdc6e26db32e80f
eemi2010 <jazzmatadazz@gmail.com>
2013-05-31 21:48:30 +0200

    Adding SlideToRemove plugin

---
+++ src/com/emitrom/touch4j/ux/dataview/slidetoremove/client/SlideToRemove.java
@@ -0,0 +1,39 @@
+package com.emitrom.touch4j.ux.dataview.slidetoremove.client;
+
+import com.emitrom.touch4j.client.core.Component;
+import com.google.gwt.core.client.JavaScriptObject;
+
+public class SlideToRemove extends Component {
+
+    @Override
+    public String getText() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setText(String text) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void setRemoveText(String value) {
+        this.setAttribute("removeText", value, true);
+    }
+
+    public void setButtonWidth(int value) {
+        this.setAttribute("buttonWidth", value, true);
+    }
+
+    @Override
+    protected void init() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    protected native JavaScriptObject create(JavaScriptObject config) /*-{
+		return new $Ext.plugin.SlideToRemove(config);
+    }-*/;
+
+}
