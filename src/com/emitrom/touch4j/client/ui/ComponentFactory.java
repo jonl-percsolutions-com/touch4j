450ce4c9169d51feaa41d2cb2d6ee9e30934e20d
eemi2010 <jazzmatadazz@gmail.com>
2013-08-03 01:45:57 +0200

    Removed unneccesary alert

--- src/com/emitrom/touch4j/client/ui/ComponentFactory.java
+++ src/com/emitrom/touch4j/client/ui/ComponentFactory.java
@@ -1,18 +1,18 @@
 /************************************************************************
-  ComponentFactory.java is part of Touch4j 4.2.2.1  Copyright 2013 Emitrom LLC
-
-  Licensed under the Apache License, Version 2.0 (the "License");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-**************************************************************************/
+ * ComponentFactory.java is part of Touch4j 4.2.2.1 Copyright 2013 Emitrom LLC
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ **************************************************************************/
 package com.emitrom.touch4j.client.ui;
 
 import com.emitrom.touch4j.client.core.Component;
@@ -20,7 +20,6 @@ import com.emitrom.touch4j.client.core.JsoHelper;
 import com.emitrom.touch4j.client.core.config.XType;
 import com.emitrom.touch4j.client.draw.Sprite;
 import com.google.gwt.core.client.JavaScriptObject;
-import com.google.gwt.user.client.Window;
 
 /**
  * Component factory class.
@@ -51,7 +50,6 @@ public class ComponentFactory {
         } else if (xtype.equalsIgnoreCase(XType.AUDIO.getValue())) {
             return new Audio(jsObj);
         } else if (xtype.equalsIgnoreCase(XType.BUTTON.getValue())) {
-            Window.alert(xtype + "??");
             return new Button(jsObj);
         } else if (xtype.equalsIgnoreCase(XType.CAROUSEL.getValue())) {
             return new Carousel(jsObj);
