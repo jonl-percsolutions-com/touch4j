3c47720f04a6c7e3b8ecc0f0027bb76a97c4e5ec
eemi2010 <jazzmatadazz@gmail.com>
2014-01-28 02:50:11 +0100

    Fixed bug : ActiveItemChangeHandler exception on null oldTab in createPeer

--- src/com/emitrom/touch4j/client/core/handlers/layout/card/ActiveItemChangeHandler.java
+++ src/com/emitrom/touch4j/client/core/handlers/layout/card/ActiveItemChangeHandler.java
@@ -1,18 +1,18 @@
 /**
- Copyright (c) 2013 Emitrom LLC. All rights reserved.
- For licensing questions, please contact us at licensing@emitrom.com
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * Copyright (c) 2013 Emitrom LLC. All rights reserved. For licensing questions,
+ * please contact us at licensing@emitrom.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.emitrom.touch4j.client.core.handlers.layout.card;
 
@@ -29,8 +29,10 @@ public abstract class ActiveItemChangeHandler extends AbstractHandler {
     private static native JavaScriptObject createPeer(ActiveItemChangeHandler listener) /*-{
 		return function(source, value, oldValue, eOpts) {
 			var container = @com.emitrom.touch4j.client.ui.Container::new(Lcom/google/gwt/core/client/JavaScriptObject;)(source);
-			var newTab = @com.emitrom.touch4j.client.ui.Container::new(Lcom/google/gwt/core/client/JavaScriptObject;)(value);
-			var oldTab = @com.emitrom.touch4j.client.ui.Container::new(Lcom/google/gwt/core/client/JavaScriptObject;)(oldValue);
+			var newTab = newTab ? @com.emitrom.touch4j.client.ui.Container::new(Lcom/google/gwt/core/client/JavaScriptObject;)(value)
+					: null;
+			var oldTab = oldTab ? @com.emitrom.touch4j.client.ui.Container::new(Lcom/google/gwt/core/client/JavaScriptObject;)(oldValue)
+					: null;
 			listener.@com.emitrom.touch4j.client.core.handlers.layout.card.ActiveItemChangeHandler::fireOnEvent(Lcom/emitrom/touch4j/client/ui/Container;Lcom/emitrom/touch4j/client/ui/Container;Lcom/emitrom/touch4j/client/ui/Container;Ljava/lang/Object;)(container, newTab, oldTab, eOpts);
 		};
     }-*/;
